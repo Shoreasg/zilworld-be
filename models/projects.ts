936ba5b0c848f396ba3152a2feb998458f1ba25d
Wei Lun <garyganweilun@gmail.com>
2023-02-11 20:48:28 +0800

    Added more paramters in projects and announcements and edited category type

--- models/projects.ts
+++ models/projects.ts
@@ -2,12 +2,20 @@ import mongoose, { Schema } from "mongoose";
 
 
 
+const announcementSchema = new Schema({
+    title: {type:String, required: true},
+    preview: {type:String, required: true},
+    
+})
+
+
 const projectsSchema = new Schema({
     name: {type:String, required: true},
     description:{type:String, required: true},
+    announcements:[announcementSchema],
     icon: {type: String, required: true},
     updated_at:{type: String},
-    category:{type:String, required: true},
+    category:[{type:String, required: true}],
     tokens:[{type:String}],
     NFT_address:[{type:String}],
     marketplace_arky:{type: Boolean},
@@ -17,6 +25,9 @@ const projectsSchema = new Schema({
     telegram: {type:String},
     twitter: {type:String},
     discord: {type:String},
+    isActive: {type: Boolean},
+    isBuilding: {type: Boolean},
+    isNew:{type: Boolean},
     
 
 })
