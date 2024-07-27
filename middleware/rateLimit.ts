2b40e8d418dd7f81fa6f2149c02bfa8f38296ba5
Shorea <garyganweilun@gmail.com>
2024-07-27 22:54:00 +0800

    fix rate limit middleware

--- middleware/rateLimit.ts
+++ middleware/rateLimit.ts
@@ -1,13 +1,6 @@
-import { rateLimit } from 'express-rate-limit';
-import { Request, Response, NextFunction } from "express";
+import { rateLimit } from "express-rate-limit";
 
-
-
-
-export const limitMiddleWare = (req: Request, res: Response, next: NextFunction) => {
-    const limiter = rateLimit({
-        windowMs: 5 * 60 * 1000, // 5 minutes
-        max: 5, // Limit each IP to 5 requests per windowMs
-    })
-    next();
-}
+export const limitMiddleWare = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // Limit each IP to 5 requests per windowMs
+});
